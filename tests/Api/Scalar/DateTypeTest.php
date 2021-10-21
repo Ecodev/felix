a550c5698ee3787e8967081b5abb27489002f685
Adrien Crivelli <adrien.crivelli@gmail.com>
2021-10-21 11:58:38 +0900

    Drop obsolete commented code

--- tests/Api/Scalar/DateTypeTest.php
+++ tests/Api/Scalar/DateTypeTest.php
@@ -36,21 +36,5 @@ final class DateTypeTest extends TestCase
         $actual = $type->parseValue('2020-03-24T23:30:00+04.5:0-30');
         self::assertInstanceOf(Date::class, $actual);
         self::assertSame('2020-03-24T00:00:00' . $timezone, $actual->format('c'), 'timezone should be ignored');
-//        $type = new DateType();
-//        $actual = $type->parseValue('2010-02-09');
-//        self::assertInstanceOf(Date::class, $actual);
-//        self::assertSame('2010-02-09T00:00:00+00:00', $actual->format('c'));
-//
-//        $actual = $type->parseValue('2010-02-09T23:00:00');
-//        self::assertInstanceOf(Date::class, $actual);
-//        self::assertSame('2010-02-09T00:00:00+00:00', $actual->format('c'));
-//
-//        $actual = $type->parseValue('2010-02-09T02:00:00+08:00');
-//        self::assertInstanceOf(Date::class, $actual);
-//        self::assertSame('2010-02-09T00:00:00+00:00', $actual->format('c'), 'timezone should be ignored');
-//
-//        $date = new Date('2010-02-03');
-//        $actual = $type->serialize($date);
-//        self::assertSame('2010-02-03', $actual);
     }
 }
