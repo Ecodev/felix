bf855eabfdefce2377bcacc4b9cec65e9b01ed9c
Sylvain Tissot <sylvain.tissot@ecodev.ch>
2022-07-25 18:16:35 +0200

    Revert "Otp code type now expects int values from the client #4579"
    This reverts commit 19e5af4b978cf945b2ae0dc78cba196eca32a9ca.

--- src/Api/Scalar/OtpType.php
+++ src/Api/Scalar/OtpType.php
@@ -4,15 +4,18 @@ declare(strict_types=1);
 
 namespace Ecodev\Felix\Api\Scalar;
 
-use GraphQL\Type\Definition\IntType;
-
-class OtpType extends IntType
+class OtpType extends AbstractStringBasedType
 {
     /**
      * @var string
      */
-    public $description = 'One time code composed of up to 6 digits';
+    public $description = 'One time passcode composed of only digits';
 
-    public const MIN_INT = 0;
-    public const MAX_INT = 999999;
+    /**
+     * Validate an OTP.
+     */
+    protected function isValid(?string $value): bool
+    {
+        return is_string($value) && preg_match('/^\d{6}+$/', $value);
+    }
 }
