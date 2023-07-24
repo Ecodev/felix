30ab2e599a9cfedb358d1c24a0fa1dac67c25008
Adrien Crivelli <adrien.crivelli@gmail.com>
2023-07-24 15:59:55 +0200

    No need to be the specialized `Api\Exception` #9696
    Because were are not inside the API yet, so that class has no purpose.

--- src/Middleware/SignedQueryMiddleware.php
+++ src/Middleware/SignedQueryMiddleware.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Ecodev\Felix\Middleware;
 
 use Cake\Chronos\Chronos;
-use Ecodev\Felix\Api\Exception;
+use Exception;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
