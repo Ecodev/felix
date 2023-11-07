2f24028174ef052a99b17a0a0b8de8698b44aceb
Adrien Crivelli <adrien.crivelli@gmail.com>
2023-11-07 13:24:00 +0900

    Typo

--- src/Api/Scalar/NonUniqueEmailType.php
+++ src/Api/Scalar/NonUniqueEmailType.php
@@ -9,7 +9,7 @@ use Ecodev\Felix\Validator\DeliverableEmail;
 /**
  * Represent an email address that can be empty string.
  *
- * This support the rare cases where an email is **not** unique in DB. And thus we want the
+ * This supports the rare cases where an email is **not** unique in DB. And thus we want the
  * field to be non-null and allow empty string to represent absence of value as we usually do
  * for string fields.
  */
