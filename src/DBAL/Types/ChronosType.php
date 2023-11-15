861d05568fb3afceb80bcefa9c7ecfc72774f8bd
Adrien Crivelli <adrien.crivelli@gmail.com>
2023-11-15 11:12:18 +0900

    ChronosDate does not implement DateTimeInterface #9967
    For coherence, we also update Chronos and cover everything with tests
    since we have more code than before.

--- src/DBAL/Types/ChronosType.php
+++ src/DBAL/Types/ChronosType.php
@@ -7,17 +7,42 @@ namespace Ecodev\Felix\DBAL\Types;
 use Cake\Chronos\Chronos;
 use DateTimeInterface;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
 use Doctrine\DBAL\Types\DateTimeType;
 
 final class ChronosType extends DateTimeType
 {
     /**
-     * @param null|DateTimeInterface|int|string $value
+     * @return ($value is null ? null : string)
+     */
+    public function convertToDatabaseValue(mixed $value, AbstractPlatform $platform): ?string
+    {
+        if ($value === null) {
+            return $value;
+        }
+
+        if ($value instanceof DateTimeInterface) {
+            return $value->format($platform->getDateTimeFormatString());
+        }
+
+        throw ConversionException::conversionFailedInvalidType($value, $this->getName(), ['null', 'Chronos']);
+    }
+
+    /**
+     * @return ($value is null ? null : Chronos)
      */
     public function convertToPHPValue(mixed $value, AbstractPlatform $platform): ?Chronos
     {
         if ($value === null || $value instanceof Chronos) {
             return $value;
+        }
+
+        if (!is_string($value) && !$value instanceof DateTimeInterface) {
+            throw ConversionException::conversionFailedFormat(
+                $value,
+                $this->getName(),
+                $platform->getDateTimeFormatString(),
+            );
         }
 
         $val = new Chronos($value);
