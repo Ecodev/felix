af00cff120b42944ca5a81b1a57e15acb7a44fca
Adrien Crivelli <adrien.crivelli@gmail.com>
2025-04-22 17:05:34 +0900

    Drop DBAL PhpEnumType in favor of ORM's own implementation #10570
    We don't need our custom implementation since ORM now supports string
    backed enum mapped to ENUM columns natively.

--- tests/Service/EnumAutoMigratorTest.php
+++ tests/Service/EnumAutoMigratorTest.php
@@ -12,7 +12,6 @@ use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Event\GenerateSchemaEventArgs;
 use Doctrine\ORM\Tools\ToolEvents;
 use Ecodev\Felix\DBAL\Types\EnumType;
-use Ecodev\Felix\DBAL\Types\PhpEnumType;
 use Ecodev\Felix\Service\EnumAutoMigrator;
 use PHPUnit\Framework\TestCase;
 
@@ -37,23 +36,15 @@ class EnumAutoMigratorTest extends TestCase
             }
         };
 
-        $phpEnumType = new class() extends PhpEnumType {
-            protected function getEnumType(): string
-            {
-                return TestEnum::class;
-            }
-        };
-
         $col1 = new Column('col1', new StringType());
         $col2 = new Column('col2', $enumType1);
         $col3 = new Column('col3', $enumType2);
         $col3bis = new Column('col3bis', $enumType2);
-        $col4 = new Column('col4', $phpEnumType);
 
         $event = new GenerateSchemaEventArgs(
             $this->createMock(EntityManager::class),
             new Schema([
-                new Table('foo', [$col1, $col2, $col3, $col3bis, $col4]),
+                new Table('foo', [$col1, $col2, $col3, $col3bis]),
             ])
         );
 
@@ -65,6 +56,5 @@ class EnumAutoMigratorTest extends TestCase
         self::assertSame('(FelixEnum:59be1fe78104fed1c6b2e6aada4faf62)', $col2->getComment());
         self::assertSame($col2->getComment(), $col3->getComment(), 'different enum that happen to have same definition have same hash, because it makes no difference for DB');
         self::assertSame('(FelixEnum:59be1fe78104fed1c6b2e6aada4faf62)', $col3bis->getComment(), 'different column with exact same type must also have same hash');
-        self::assertSame('(FelixEnum:fa38e8669a8a21493a62a0d493a28ad0)', $col4->getComment(), 'native PHP enum are supported too');
     }
 }
